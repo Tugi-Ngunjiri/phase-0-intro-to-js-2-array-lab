aa36c5e2c566c56e86276244eaa8c0e2cc22d757
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-24 23:36:10 +0300

    done with my assigment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+"before all" hook in "{root}"
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
