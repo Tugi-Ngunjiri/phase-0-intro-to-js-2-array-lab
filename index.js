b36e9bde12726911d7dc446236c812e5b909b37a
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-25 09:59:22 +0300

    done

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 "before all" hook in "{root}"
 beforeEach(function () {
     cats.length = 0;1,2,3,4,5
-  
     cats.push("Milo", "Otis", "Garfield");
   });
   beforeEach(function () {
     cats.length = 0;1,2,3,4,5
-  
+
     cats.push("Milo", "Otis", "Garfield");
   });
   beforeEach(function () {
     cats.length = 0;1,2,3,4,5
-  
+
     cats.push("Milo", "Otis", "Garfield");
   });
